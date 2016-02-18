cebc54546b44d6067184d4dacf15f7514bebabda
Nicole Grima <nicolegrima@Nicoles-MacBook-Air.local>
2016-02-18 09:27:16 -0800

    Fix gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,14 @@
 var gulp = require('gulp');
-// will go to node modules folder - find gulp, pulls in code
-
 var browserSync = require('browser-sync').create();
-// auto Sync the browswer when changes are made
-
 var plumber = require('gulp-plumber');
-// prevent pipe breaking caused by errors from gulp plugins
-
 var notify = require('gulp-notify');
-// to send messages based on Vinyl Files or Errors to Mac OS X, Linux or Windows using the node-notifier module.
-// gulp-notify passes on the vinyl files even on error. So if you are using gulp-plumber the run will not break if the notifier returns an error.
-//If you want to notify on errors gulp-plumber can be used to not break the run and force you to have to restart gulp.
-
 var rename = require("gulp-rename");
-// to rename files easily.
-
 var jscs = require('gulp-jscs');
-// check JavaScript code style with jscs + report
-
 var babel = require('gulp-babel');
 
 var sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'), //this will take care of vendor prefixes
+    autoprefixer = require('gulp-autoprefixer'),
     minifyCSS = require('gulp-cssnano');
-
 
 gulp.task('sass', function() {
    gulp.src('./sass/style.scss')
@@ -36,20 +21,19 @@ gulp.task('sass', function() {
 });
 
 gulp.task('compile-react', function(){
-    return gulp.src('main.jsx') // What files do we want gulp to consume? The source
+    return gulp.src('main.jsx')
         .pipe(jscs()) // run jscs
-        .pipe(jscs.reporter()) // reports errors - put ahead of Uglify always. Helped when put ahead of plumber
+        .pipe(jscs.reporter())
         .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(rename('main.min.js')) // Put after uglify
+        .pipe(rename('main.min.js'))
 				.pipe(browserify({
 					insertGlobals: true,
 					debug: true
 				}))
-				.pipe(gulp.dest('./build')); // Where do we put the result? The destination
+				.pipe(gulp.dest('./build'));
 });
-// use the ./ for this to work
 
-gulp.task('watch', ['browser-sync'],['compile-react'], function(){
+gulp.task('browser-sync', function(){
 
     browserSync.init({
       server: {
@@ -57,11 +41,9 @@ gulp.task('watch', ['browser-sync'],['compile-react'], function(){
       }
 });
 
-gulp.watch(['main.jsx'], ['compile-react']);
-gulp.watch(['./build/script.js'], ['index.html']).on('change', browserSync.reload);
-gulp.watch(['./sass/style.scss'], ['sass']);
+gulp.watch('main.jsx', ['compile-react']);
+gulp.watch(['./build/script.js', 'index.html', './build/css/*.min.css']).on('change', browserSync.reload);
+gulp.watch('./sass/style.scss', ['sass']);
 });
-// in command line - type gulp watch and the main js file gets uglified, and the browser with index reloads
 
-gulp.task('default', ['browser-sync'],['watch']);
-// thanks to above you can just write in watch only and it'll trigger the above watch function lines
+gulp.task('default', ['browser-sync']);
--- main.jsx
+++ main.jsx
@@ -1,21 +1,24 @@
-// 'use strict';
-//
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-//
-// var App = React.createClass ({
-//
-//
-// render: function(){
-//   return (
-//     <div></div>
-//
-//
-//   );
-// }
-// });
-//
-//
-//
-// // Load to DOM in <div of class name .mount-node>
-// ReactDOM.render(<App />, document.querySelector('.mount-node'));
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass ({
+
+
+render: function(){
+  return (
+    <div>
+      <div class="clock">The Clock goes here</THE></div>
+      <button>Take Test</button>
+    </div>
+
+
+  );
+}
+});
+
+
+
+// Load to DOM in <div of class name .mount-node>
+ReactDOM.render(<App />, document.querySelector('.mount-node'));
